4049316f986942f9b39a6f0ab1a613b2bdba0d12
Saeed Balquizi (Alfalasi) <125722449+falasi@users.noreply.github.com>
2023-08-15 20:04:22 -0400

    Update race.py

--- Burp/TurboIntruder/race.py
+++ Burp/TurboIntruder/race.py
@@ -1,4 +1,5 @@
 # Old version of race.py https://github.com/PortSwigger/turbo-intruder/commit/d3fc0df14df987579ddaba172eabc240962862bf
+# https://forum.portswigger.net/thread/turbo-intruder-race-py-script-missing-e9a944cc
 def queueRequests(target, wordlists):
     engine = RequestEngine(endpoint=target.endpoint,
                            concurrentConnections=30,
